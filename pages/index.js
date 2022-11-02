c83f4bdd4b5b81a15f9970f9dbffcb4858e1ac88
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-11-02 13:09:41 +0530

    main component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Main from "../components/Main";
 import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../components/Navbar";
@@ -11,6 +12,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Navbar />
+			<Main />
 		</div>
 	);
 }
