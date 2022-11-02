cfb0cf46b3631885513ae1c18dbc06f0a25ae182
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-11-02 12:40:08 +0530

    navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
 	const handleNav = () => {
 		setNav(!nav);
 	};
+
 	return (
 		<div className="fixed w-full h-20 shadow-xl z-[100]">
 			<div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
