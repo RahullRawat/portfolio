e2732127abd262bc17b4dd711e440d52b60fa28d
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-11-05 13:28:44 +0530

    changed about desc

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import AboutImg from "../public/assets/about.jpg";
 
@@ -10,20 +11,26 @@ const About = () => {
 					<p className="text-xl tracking-widest text-[#5651e5] uppercase">
 						About
 					</p>
-					<h2 className="py-4">Who I Am</h2>
-					<p className="py-2 text-gray-600">
-						I’m a FrontEnd Developer with experience in building
-						high-performing, responsive, beautiful web applications to deliver a
-						unique, intuitive user experience. I enjoy being challenged and
-						working on projects that require me to step outside of my comfort
-						zone.
+					<h2 className="py-4 text-gray-600">Who I Am</h2>
+					<p className="py-2 text-gray-600 tracking-widest leading-8">
+						I specialize in building mobile responsive front-end UI applications
+						that connect with API’s and other backend technologies. I’m
+						passionate about learning new technologies and understand there is
+						more than one way to accomplish a task. Though I am most proficient
+						in building front-end applications using HTML, CSS, Javascript, and
+						React, I am a quick learner and can pick up new tech stacks as
+						needed. I believe that being a great developer is not using one
+						specific language, but choosing the best tool for the job.
 					</p>
-					<p className="py-2 text-gray-600 underline cursor-pointer">
-						Check out some of my latest projects.
-					</p>
+
+					<Link href="/#projects">
+						<p className="py-2 text-gray-600 underline cursor-pointer">
+							Check out some of my latest projects.
+						</p>
+					</Link>
 				</div>
-				<div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-					<Image src={AboutImg} alt="/" className="rounded-xl" />
+				<div className="w-full h-[25rem] m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+					<Image src={AboutImg} alt="/" className="rounded-xl h-full" />
 				</div>
 			</div>
 		</div>
